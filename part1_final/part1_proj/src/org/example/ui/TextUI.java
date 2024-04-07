248aab564f6cfa61189ec193939a247252da81c2
boulce <boulce.email@gmail.com>
2024-04-07 13:43:13 +0900

    feat: impl delete a tuple
    impl, required to refactor

--- part1_final/part1_proj/src/org/example/ui/TextUI.java
+++ part1_final/part1_proj/src/org/example/ui/TextUI.java
@@ -75,6 +75,124 @@ public class TextUI {
                     dmlOrganizer.insertRecord(relationMetadata, recordToInsert, attributeMetadataList);
 
                 } else if (Objects.equals(command, "3")) {
+                    // Get relation metadata
+                    RelationMetadata relationMetadata = getValidRelationMetadata(scanner, conn, dmlOrganizer);
+
+                    // Get attribute metadata list
+                    ArrayList<AttributeMetadata> attributeMetadataList = dmlOrganizer.getAttributeMetadataForQuery(conn, relationMetadata);
+
+                    // Get the attribute position of primary key
+                    List<Integer> attPosOfPrimaryKey = dmlOrganizer.getAttPosOfPrimaryKey(attributeMetadataList);
+
+                    HashMap<Integer, String> primaryKeyMap = getPrimaryKeyMapForDelete(attPosOfPrimaryKey, attributeMetadataList, scanner);
+
+                    // Read from the file
+                    List<Record> resultSet = new ArrayList<>();
+                    RandomAccessFile file = new RandomAccessFile(relationMetadata.getLocation() + relationMetadata.getRelationName() + ".tbl", "rw");
+
+
+                    // TODO DMLOrganizer에서 getRecordSize로 묶기
+                    int recordSize = 0;
+                    for (AttributeMetadata attributeMetadata : attributeMetadataList) {
+                        recordSize += attributeMetadata.getLength();
+                    }
+                    recordSize += 4;
+                    int blockSize = recordSize * BlockingFactor.VAL;
+
+                    byte[] headerBlockBytes = new byte[blockSize];
+                    file.read(headerBlockBytes);
+                    Block headerBlock = new Block(0, headerBlockBytes, attributeMetadataList);
+
+                    file.seek(0);
+                    Block readBlock = null;
+                    int recordIdx = 0; // Record Index to be deleted
+                    for(int blockIdx = 0; ; blockIdx++){
+
+                        byte[] readBlockBytes = new byte[blockSize];
+                        int readCnt = file.read(readBlockBytes);
+
+                        if(readCnt == -1) { // EOF
+                            break;
+                        }
+
+                        readBlock = new Block(blockIdx, readBlockBytes, attributeMetadataList);
+
+                        for(recordIdx = 0; recordIdx < BlockingFactor.VAL; recordIdx++) {
+                            Record readRecord = readBlock.getRecords()[recordIdx];
+
+                            // If some attribute of primary-key of a record is NULL, it is a deleted record.
+                            // So don't contain the deleted record to the result set
+                            boolean isDeleted = false;
+                            for(int pos : primaryKeyMap.keySet()) {
+                                if (isNullAttribute(readRecord.getAttributes().get(pos))){
+                                    isDeleted = true;
+                                    break;
+                                }
+                            }
+                            if(!isDeleted) {
+                                boolean found = true;
+                                for (Integer key : primaryKeyMap.keySet()) {
+                                    if(!new String(readRecord.getAttributes().get(key)).trim().equals(primaryKeyMap.get(key))) { // compare search key and read record attribute
+                                        found = false;
+                                        break;
+                                    }
+                                }
+                                if(found) { // Found the block to be deleted
+                                    resultSet.add(readRecord);
+                                    break;
+                                }
+                            }
+                        }
+
+                        if(!resultSet.isEmpty()){ // Found the block to be deleted
+                            break;
+                        }
+                    }
+
+                    if(!resultSet.isEmpty()){ // Found the block to be deleted
+
+                        Record recordToDelete = resultSet.get(0);
+                        // Make the record to be deleted the null record
+                        List<char[]> nullAtts = recordToDelete.getAttributes().stream().map(att -> new char[att.length]).collect(Collectors.toList());
+
+                        if(headerBlock.getIdx() == readBlock.getIdx()) { // If block that contains the record to be deleted is same to header block
+                            recordToDelete.setAttributes(nullAtts);
+                            recordToDelete.setLink(0);
+
+                            Record headerRecord = readBlock.getRecords()[0];
+                            // Set the link of record to be deleted as the link of header
+                            recordToDelete.setLink(headerRecord.getLink());
+
+                            // Set the link of header as byte offset of record to be deleted
+                            headerRecord.setLink(readBlock.getIdx()*blockSize + recordIdx*recordSize);
+
+                            file.seek(readBlock.getIdx()*blockSize);
+                            file.write(readBlock.getByteArray());
+                        } else { // If block that contains the record to be deleted is not same to header block
+                            recordToDelete.setAttributes(nullAtts);
+                            recordToDelete.setLink(0);
+
+                            Record headerRecord = headerBlock.getRecords()[0];
+                            // Set the link of record to be deleted as the link of header
+                            recordToDelete.setLink(headerRecord.getLink());
+
+                            // Set the link of header as byte offset of record to be deleted
+                            headerRecord.setLink(readBlock.getIdx()*blockSize + recordIdx*recordSize);
+
+                            file.seek(headerBlock.getIdx()*blockSize);
+                            file.write(headerBlock.getByteArray());
+
+                            file.seek(readBlock.getIdx()*blockSize);
+                            file.write(readBlock.getByteArray());
+                        }
+
+                    } else { // Not found the block to be deleted
+                        System.out.println("[ERROR] There isn't the record that has the primary key you entered.");
+                        System.out.println();
+                    }
+
+                    file.close();
+
 
                 } else if (Objects.equals(command, "4")) {
                     // Get relation metadata
@@ -98,7 +216,7 @@ public class TextUI {
                     // Get the attribute position of primary key
                     List<Integer> attPosOfPrimaryKey = dmlOrganizer.getAttPosOfPrimaryKey(attributeMetadataList);
 
-                    HashMap<Integer, String> primaryKeyMap = getPrimaryKeyMap(attPosOfPrimaryKey, attributeMetadataList, scanner);
+                    HashMap<Integer, String> primaryKeyMap = getPrimaryKeyMapForSearch(attPosOfPrimaryKey, attributeMetadataList, scanner);
 
                     List<Record> resultSetForSelectOne = dmlOrganizer.getResultSetForSelectOne(relationMetadata, attributeMetadataList, primaryKeyMap);
 
@@ -321,12 +439,34 @@ public class TextUI {
         return relationMetadata;
     }
 
-    private static HashMap<Integer, String> getPrimaryKeyMap(List<Integer> attPosOfPrimaryKey, ArrayList<AttributeMetadata> attributeMetadataList, Scanner scanner) {
+    private static HashMap<Integer, String> getPrimaryKeyMapForSearch(List<Integer> attPosOfPrimaryKey, ArrayList<AttributeMetadata> attributeMetadataList, Scanner scanner) {
         HashMap<Integer, String> primaryKeyMap = new HashMap<>(); // { primary_key_attribute, value }
 
         for (Integer pos : attPosOfPrimaryKey) {
             while(true) {
                 System.out.printf("Enter the search key of primary key attribute '" + attributeMetadataList.get(pos).getAttributeName() + "' (type: "+ attributeMetadataList.get(pos).getDomainType()+", length: " + attributeMetadataList.get(pos).getLength()+"): ");
+                String key = scanner.next();
+                System.out.println();
+
+                if(key.length() > attributeMetadataList.get(pos).getLength()) {
+                    System.out.println("[ERROR] Your input is bigger than the attribute size. Try again.");
+                    System.out.println();
+                }
+                else {
+                    primaryKeyMap.put(pos, key);
+                    break;
+                }
+            }
+        }
+        return primaryKeyMap;
+    }
+
+    private static HashMap<Integer, String> getPrimaryKeyMapForDelete(List<Integer> attPosOfPrimaryKey, ArrayList<AttributeMetadata> attributeMetadataList, Scanner scanner) {
+        HashMap<Integer, String> primaryKeyMap = new HashMap<>(); // { primary_key_attribute, value }
+
+        for (Integer pos : attPosOfPrimaryKey) {
+            while(true) {
+                System.out.printf("Enter the search key of primary key attribute to delete '" + attributeMetadataList.get(pos).getAttributeName() + "' (type: "+ attributeMetadataList.get(pos).getDomainType()+", length: " + attributeMetadataList.get(pos).getLength()+"): ");
                 String key = scanner.next();
                 System.out.println();
 
